a1dc51d0e3d624e66e066c7f2e923bc1f52a0edb
Vitor e Rafael <vitorsouzalibrelonrestini@gmail.com>
2024-04-16 11:33:56 -0300

    Projeto finalizado

--- CaraCoroa/CaraCoroa/MainPage.xaml.cs
+++ CaraCoroa/CaraCoroa/MainPage.xaml.cs
@@ -14,6 +14,17 @@
         {
             String verif = Convert.ToString(Picker.SelectedItem);
             ResultHeadTail.Text = verif;
+            int imag = rdn.Next(0, 1 + 1);
+            if (imag == 0)
+            {
+                ResultHeadTail.Text = "Cara";
+                ImgHeadTails.Source = "moeda_cara.jpeg";
+            }
+            else
+            {
+                ResultHeadTail.Text = "Coroa";
+                ImgHeadTails.Source = "moeda_coroa.jpg";
+            }
         }
     }
 
