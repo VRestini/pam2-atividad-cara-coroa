679987ddfad9b74218bdf36f0a2e3f58fc0e5b1e
Vitor <vitorsouzalibrelonrestini@gmail.com>
2024-04-15 22:11:34 -0300

    Criação dos componentes e parte do funcionamento criado e funcionando

---
+++ CaraCoroa/CaraCoroa/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace CaraCoroa
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        Random rdn = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_ClickedHeadTails(object sender, EventArgs e)
+        {
+            String verif = Convert.ToString(Picker.SelectedItem);
+            ResultHeadTail.Text = verif;
+        }
+    }
+
+}
